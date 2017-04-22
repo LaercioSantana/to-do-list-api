84fb84b082dd9625f08d5a5ca36543114e7f3ccd
Laercio Santana <laerciosantana021@hotmail.com>
2017-04-22 00:27:19 -0300

    make seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create([
+  {
+    full_name: "user1",
+    email: "user1@email.com",
+    password: "pass1"
+  },
+  {
+    full_name: "user2",
+    email: "user2@email.com",
+    password: "pass2"
+  },
+  {
+    full_name: "user3",
+    email: "user3@email.com",
+    password: "pass3"
+  }
+])
+
+Todo.create([
+    {
+      description: "description todo 1 user 1",
+      user_id: 1
+    },
+    {
+      description: "description todo 1 user 1",
+      user_id: 1
+    },
+    {
+      description: "description todo 2 user 2",
+      user_id: 2
+    },
+    {
+      description: "description todo 2 user 2",
+      user_id: 2
+    },
+    {
+      description: "description todo 3 user 2",
+      user_id: 2
+    },
+])
