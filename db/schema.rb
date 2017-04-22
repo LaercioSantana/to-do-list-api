42f15dc2fa02d08a809ec26eb8da67663c131a21
Laercio Santana <laerciosantana021@hotmail.com>
2017-04-22 04:45:50 -0300

    migrate to postgresql because heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170422045811) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.boolean  "done",        default: false
     t.text     "description"
@@ -19,7 +22,7 @@ ActiveRecord::Schema.define(version: 20170422045811) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.integer  "user_id"
-    t.index ["user_id"], name: "index_todos_on_user_id"
+    t.index ["user_id"], name: "index_todos_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,7 +32,7 @@ ActiveRecord::Schema.define(version: 20170422045811) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "token"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
 end
