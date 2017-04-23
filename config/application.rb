513acac31c4350f8519f64600cf1a7772eba8ff6
Laercio Santana <laerciosantana021@hotmail.com>
2017-04-22 22:20:39 -0300

    provides support for CORS

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,14 @@ module ToDoListApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    #Rack::Cors to provides support for Cross-Origin Resource Sharing (CORS)
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete, :put]
+      end
+    end
+
   end
 end
